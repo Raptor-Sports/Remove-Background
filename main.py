34d45b3c00ca04ea19068adf3242d54d9dc9183e
lwfe <leonardolwfe@gmail.com>
2023-09-19 22:33:21 -0300

    refactor: change flask to fastapi

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
+from fastapi import FastAPI, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
 from io import BytesIO
-from flask import Flask, request, jsonify, send_file
-from tempfile import NamedTemporaryFile
 from rembg import remove
 from PIL import Image
 
-app = Flask(__name__)
+app = FastAPI()
 
 
-@app.route('/removeBackground', methods=['POST'])
-def remove_background():
+@app.post('/removeBackground')
+async def remove_background(image: UploadFile):
     try:
-        if 'image' not in request.files:
-            return jsonify({'error': 'No image file uploaded'}), 400
+        if not image:
+            raise HTTPException(
+                status_code=400, detail='No image file uploaded')
 
-        with NamedTemporaryFile(delete=False, suffix=".png") as temp_image:
-            image_file = request.files['image']
-            image_file.save(temp_image)
+        with BytesIO() as temp_image:
+            temp_image.write(await image.read())
+            temp_image.seek(0)
 
-        input = Image.open(temp_image.name)
-        output_image = remove(input)
+            input_image = Image.open(temp_image)
+            output_image = remove(input_image)
 
-        output_image_bytes = BytesIO()
-        output_image.save(output_image_bytes, format="PNG")
-        output_image_bytes.seek(0)
+            output_image_bytes = BytesIO()
+            output_image.save(output_image_bytes, format="PNG")
+            output_image_bytes.seek(0)
 
-        return send_file(output_image_bytes, mimetype='image/png')
+            return StreamingResponse(output_image_bytes, media_type='image/png')
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+        raise HTTPException(status_code=500, detail=str(e))
